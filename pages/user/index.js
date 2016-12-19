a2dfc60632e3224b1a372d00d5474427ca644671
skyvow <724355924@qq.com>
2016-12-19 18:51:19 +0800

    add help

--- pages/user/index.js
+++ pages/user/index.js
@@ -22,27 +22,27 @@ Page({
 			{
 				icon: '../../assets/images/iconfont-help.png',
 				text: '常见问题',
+				path: '/pages/help/list/index',
 			},
 		],
+		settings: [
+			{
+				icon: '../../assets/images/iconfont-clear.png',
+				text: '清除缓存',
+				path: '0.0KB'
+			}, 
+			{
+				icon: '../../assets/images/iconfont-about.png',
+				text: '关于我们',
+				path: '/pages/about/index'
+			}, 
+		]
 	},
 	onLoad() {
-		const that = this
-		const userInfo = App.globalData.userInfo
-
-		if (userInfo) {
-			that.setData({
-				userInfo: userInfo
-			})
-			return
-		}
-
-		App.getUserInfo()
-		.then(data => {
-			console.log(data)
-			that.setData({
-				userInfo: data
-			})
-		})
+		this.getUserInfo()
+	},
+	onShow() {
+		this.getStorageInfo()
 	},
 	navigateTo(e) {
 		const index = e.currentTarget.dataset.index
@@ -53,6 +53,49 @@ Page({
 				App.WxService.makePhoneCall({
 					phoneNumber: path
 				})
+				break
+			default:
+				App.WxService.navigateTo(path)
+		}
+    },
+    getUserInfo() {
+    	const userInfo = App.globalData.userInfo
+
+		if (userInfo) {
+			this.setData({
+				userInfo: userInfo
+			})
+			return
+		}
+
+		App.getUserInfo()
+		.then(data => {
+			console.log(data)
+			this.setData({
+				userInfo: data
+			})
+		})
+    },
+    getStorageInfo() {
+    	App.WxService.getStorageInfo()
+    	.then(data => {
+    		console.log(data)
+    		this.setData({
+    			'settings[0].path': `${data.currentSize}KB`
+    		})
+    	})
+    },
+    bindtap(e) {
+    	const index = e.currentTarget.dataset.index
+		const path = e.currentTarget.dataset.path
+
+		switch(index) {
+			case '0':
+				App.WxService.showModal({
+		            title: '友情提示', 
+		            content: '确定要清除缓存吗？', 
+		        })
+		        .then(data => data.confirm == 1 && App.WxService.clearStorage())
 				break
 			default:
 				App.WxService.navigateTo(path)
